7de2a68ec2350c371e543a5b976aa96cc47d37de
Student 8 <Student 8@10.8.33.163>
2017-01-23 13:16:33 -0500

    creation

---
+++ dragonLand/src/game/DragonLand.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package game;
+
+import guiPractice.GUIApplication;
+
+/**
+ * @author Kat
+ *
+ */
+public class DragonLand extends GUIApplication {
+
+	/**
+	 * 
+	 */
+	public DragonLand() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see guiPractice.GUIApplication#initScreen()
+	 */
+	@Override
+	protected void initScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
