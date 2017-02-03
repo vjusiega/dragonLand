10aa2bec19fa10b92d9fd7003e05d6891b324cba
Student 8 <Student 8@10.8.33.168>
2017-02-03 13:58:36 -0500

    buttons

--- dragonLand/src/game/ShopScreen.java
+++ dragonLand/src/game/ShopScreen.java
@@ -17,7 +17,7 @@ public class ShopScreen extends ClickableScreen {
 	}
 
 	@Override
-	public void initAllObjects(ArrayList<Visible> arg0) {
+	public void initAllObjects(ArrayList<Visible> visible) {
 //		Button exit = new Button(getWidth() - 60,  10, 50, 50, "X", Color.RED, new Action(){
 //			public void Act()
 //			{
@@ -28,10 +28,27 @@ public class ShopScreen extends ClickableScreen {
 		int titleWidth = 100;
 		int titleHeight = 65;
 		
-		TextLabel shopTitle = new TextLabel(getWidth()/2 - titleWidth, getHeight()/2 - titleHeight, titleWidth, titleHeight, text);
+		TextLabel shopTitle = new TextLabel(getWidth()/2 - titleWidth/2, getHeight()/2 - titleHeight/2, titleWidth, titleHeight, "Dragon Shop");
 		
-		Button buy = new Button(x, y, w, h, text, color, action);
-		Button sell = new Button(x, y, w, h, text, color, action);
+		int buttonWidth = 90;
+		int buttonHeight = 55;
+		
+		Button buy = new Button(getWidth()/2 - buttonWidth/2, getHeight()/2 - buttonHeight/2, buttonWidth, buttonHeight, "BUY", Color.cyan, new Action(){
+			public void act()
+			{
+				
+			}
+		});
+		Button sell = new Button(getWidth()/2 - buttonWidth/2, getHeight()/2 - buttonHeight/2, buttonWidth, buttonHeight, "SELL", Color.cyan, new Action(){
+			public void act()
+			{
+				
+			}
+		});
+		
+		addObject(shopTitle);
+		addObject(buy);
+		addObject(sell);
 	}
 
 }
