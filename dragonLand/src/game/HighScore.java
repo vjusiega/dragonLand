6aa22956a4f6876455b46e82247d57491d3b63e3
Veeraj <Period82.0@10.8.33.160>
2017-02-03 14:01:54 -0500

    Created high score screen

---
+++ dragonLand/src/game/HighScore.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package game;
+
+/**
+ * @author Veeraj
+ *
+ */
+public class HighScore {
+
+	/**
+	 * 
+	 */
+	public HighScore() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
