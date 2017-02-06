26ecafece684cfb4a1880810138f89580a46932e
Student6 <Student6@10.8.33.148>
2017-02-06 12:54:32 -0500

    Created a class: BuyScreenW

---
+++ dragonLand/src/game/BuyScreenW.java
@@ -0,0 +1,23 @@
+package game;
+
+import java.util.ArrayList;
+
+import guiPractice.ClickableScreen;
+import guiPractice.components.Visible;
+
+public class BuyScreenW extends ClickableScreen {
+
+    private ArrayList<Dragons> dragonsInShop;
+
+	public BuyScreenW(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initAllObjects(ArrayList<Visible> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
