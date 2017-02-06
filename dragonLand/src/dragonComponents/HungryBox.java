cbe40a3bbca43382cc60ef921c7b45b7e19a0c79
Student 8 <Student 8@10.8.33.159>
2017-02-06 12:52:15 -0500

    Hungry Box Beginning

---
+++ dragonLand/src/dragonComponents/HungryBox.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package dragonComponents;
+
+import java.awt.Color;
+
+import guiPractice.components.Action;
+import guiPractice.components.Button;
+
+/**
+ * @author Jenniber Franco
+ *
+ */
+public class HungryBox extends Button {
+
+	private static final int W = 50;
+	private static final int H = 25;
+	private static final String TEXT = "Hungry!";
+	
+	
+	/**
+	 * @param x
+	 * @param y
+	 * @param w
+	 * @param h
+	 * @param text
+	 * @param color
+	 * @param action
+	 */
+	public HungryBox(int x, int y, Color color, Action action) {
+		super(x, y, W, H, TEXT, color, new Action);
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
