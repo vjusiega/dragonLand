1a9b4670c3450d5275ecc63f0edc677698292d61
Veeraj Jethalal <veera@DESKTOP-0ECFG49>
2017-02-07 23:50:45 -0500

    Added update method (Doesn't work)

--- dragonLand/src/game/HighScoreScreen.java
+++ dragonLand/src/game/HighScoreScreen.java
@@ -4,7 +4,11 @@
 package game;
 
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import guiPractice.ClickableScreen;
 import guiPractice.components.Action;
@@ -18,6 +22,7 @@ import guiPractice.components.TextLabel;
 public class HighScoreScreen extends ClickableScreen implements MouseListener{
 	
 	private TextLabel test;
+	private BufferedImage image;
 	/**
 	 * 
 	 */
@@ -27,8 +32,21 @@ public class HighScoreScreen extends ClickableScreen implements MouseListener{
 
 	@Override
 	public void initAllObjects(ArrayList<Visible> viewObjects) {
-		test = new TextLabel(40, 40, 150, 20, "This is a test");
+		System.out.println(getWidth());
+		test = new TextLabel((getWidth()/2)-100, 50, 150, 20, "High Scores");
 		viewObjects.add(test);
+		//update();
 	}
-
+	
+//	public void update(){
+//		image = new BufferedImage(getWidth(), getHeight(), 
+//				BufferedImage.TYPE_INT_ARGB);
+//		Graphics2D g = image.createGraphics();
+//		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+//				RenderingHints.VALUE_ANTIALIAS_ON);
+//		g.setColor(Color.white);
+//		g.fillRect(0, 0, image.getWidth(), image.getHeight());
+//		g.setColor(Color.red);
+//		g.fillRect(40,40, 150, 20);
+//	}
 }
