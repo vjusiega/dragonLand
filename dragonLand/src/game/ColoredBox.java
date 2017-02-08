7d160bd852bdf8e174bf47f6c6a6b7d8c17c0f15
Veeraj <Period82.0@10.8.33.160>
2017-02-08 13:01:04 -0500

    d

---
+++ dragonLand/src/game/ColoredBox.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package game;
+
+import java.awt.Graphics2D;
+
+import guiPractice.components.Component;
+
+/**
+ * @author Period82.0
+ *
+ */
+public class ColoredBox extends Component {
+
+	/**
+	 * @param x
+	 * @param y
+	 * @param w
+	 * @param h
+	 */
+	public ColoredBox(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see guiPractice.components.Component#update(java.awt.Graphics2D)
+	 */
+	@Override
+	public void update(Graphics2D arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
