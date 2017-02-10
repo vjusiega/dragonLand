1d64cfa522321d93daa7062ca3616629b6598200
Veeraj <Period82.0@10.8.33.160>
2017-02-10 12:56:10 -0500

    Changes

--- dragonLand/src/game/HighScoreScreen.java
+++ dragonLand/src/game/HighScoreScreen.java
@@ -37,6 +37,7 @@ public class HighScoreScreen extends ClickableScreen implements MouseListener{
 	private Button coinsWon;
 	private Button scores;
 	private Button totalCoins;
+	private ArrayList highScores;
 	/**
 	 * 
 	 */
@@ -46,21 +47,16 @@ public class HighScoreScreen extends ClickableScreen implements MouseListener{
 
 	@Override
 	public void initAllObjects(ArrayList<Visible> viewObjects) {
-		yourScore = new Button(345, 175, 300, 50, "Your score: " + GameScreen.getScore(), DragonLand.DARKER_NUDE, null);
+		yourScore = new Button(345, 190, 300, 50, "Your score: " + GameScreen.getScore(), DragonLand.DARKER_NUDE, null);
 		yourScore.setSize(30);
-		layerOne = new CurvedButton(50,70,getWidth()-100, getHeight()-110,null,DragonLand.LIGHT_NUDE, null);
-		layerTwo = new Button(100, 150, getWidth()-200, getHeight()-240, null, DragonLand.BRIGHT_PINK, null);
-		title = new Button((getWidth()/2)-125, 40, 250, 60, "High Scores", DragonLand.DARKER_NUDE, null);
+		layerOne = new CurvedButton(50,70,getWidth()-100, getHeight()-95,null,DragonLand.LIGHT_NUDE, null);
+		layerTwo = new Button(100, 150, getWidth()-200, getHeight()-225, null, DragonLand.BRIGHT_PINK, null);
+		title = new Button((getWidth()/2)-125, 80, 250, 60, "High Scores", DragonLand.DARKER_NUDE, null);
 		title.setSize(40);
 		background = new Background(0,0, getWidth(), getHeight());
-		coinsWon = new Button(345, 450, 300, 60, "Coins Won: WON", DragonLand.DARKER_NUDE, new Action(){
-			@Override
-			public void act(){
-				
-			}
-		});
+		coinsWon = new Button(345, 465, 300, 60, "Coins Won: WON", DragonLand.DARKER_NUDE, null);
 		coinsWon.setSize(30);
-		scores = new Button(295,237,400,200,"Scores", DragonLand.DARKER_NUDE,null);
+		scores = new Button(295,252,400,200,"Scores", DragonLand.DARKER_NUDE,null);
 		totalCoins = new Button(700,87,180,50,"Total Coins: COINS", DragonLand.DARKER_NUDE,null);
 		back = new Button(50,50, 40, 40, "X", DragonLand.DARKER_NUDE, new Action(){	
 
