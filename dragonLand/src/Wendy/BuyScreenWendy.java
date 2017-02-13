13fe40dd70e5f1959e92e093bfb023a65cbaeb3c
user <user@Win10>
2017-02-12 22:23:59 -0500

    new BuyScreenWendy extends ShopScreen

---
+++ dragonLand/src/Wendy/BuyScreenWendy.java
@@ -0,0 +1,15 @@
+package Wendy;
+
+import game.ShopScreen;
+import guiPractice.components.Action;
+
+public class BuyScreenWendy extends ShopScreen{
+
+	public BuyScreenWendy(int width, int height, Action act) {
+		super(width, height, act);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
