cb35574e5b9610241a22d99e345edd1dcd7a6c80
Wendy Hu <whu9517@bths.edu>
2017-02-13 14:47:20 -0500

    Changed update and run method

--- dragonLand/src/dragonComponents/HungryBox.java
+++ dragonLand/src/dragonComponents/HungryBox.java
@@ -33,29 +33,21 @@ public class HungryBox extends Button implements Runnable {
 	 * @param action
 	 */
 	public HungryBox(int x, int y, Color color) {
-		super(x, y, W, H, TEXT+"/n"+hungryTime+" sec", color, null);
+		super(x, y, W, H, TEXT+"\n"+hungryTime+" sec", color, null);
 	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 	
-	public void createHungryThread(){
-		HungryBox hungry1 = new HungryBox(HomeKat.dragonsOnScreen.getX(),HomeKat.dragonsOnScreen.getY()+100,DragonLand.DARKER_NUDE);
-		Thread hungry = new Thread(this);
+	public void createHungryThread(Dragon d){
+		//d is a dragon from HomeKat.onScreenDragons
+		HungryBox hungryDragon = new HungryBox(d.getX(),d.getY()+100,DragonLand.DARKER_NUDE);
+		Thread hungry = new Thread(hungryDragon);
 		hungry.start();
 	}
 	
 	private void changeHungryTime(){
 		try{
-			hungryTime--;
-			Thread.sleep(1000);
-			if(hungryTime<=0){
-				Thread.;
+			while(hungryTime>=0){
+				Thread.sleep(1000);
 			}
 		}catch(InterruptedException e){
 			e.printStackTrace();
@@ -64,11 +56,42 @@ public class HungryBox extends Button implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		changeHungryTime();
+		hungryTime--;
 	}
 	
-	
+	@Override
+	public void update(Graphics2D g){
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		if(hungryTime<5){
+			g.setColor(DragonLand.LIGHT_PINK);
+		}
+		else{
+			g.setColor(DragonLand.DARKER_NUDE);
+		}
+		double thickness = 2;
+		Stroke oldStroke = g.getStroke();
+		g.setStroke(new BasicStroke((float) thickness));
+		g.fillRoundRect(0, 0, getWidth(), getHeight(), 35, 25);
+		g.setColor(DragonLand.NAVY);
+		g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 35, 25);
+		
+		g.setFont(new Font("Dialog",Font.BOLD,getSize()));
+		FontMetrics fm = g.getFontMetrics();
+		
+		if(getText()!= null){
+			
+			String t = getText();
+			//just in case text is too wide, cut off
+			int cutoff = t.length();
+			while(cutoff > 0 && fm.stringWidth(t) > getWidth()){
+				cutoff --;
+				t = t.substring(0,cutoff); 
+			}
+			g.drawString(t, (getWidth()-fm.stringWidth(t))/2, (getHeight()+fm.getHeight()-fm.getDescent())/2);
+		}
+	}
 	
 //	public void addHungry() {
 //		for(int i= 0; i<hungryTimes.size(); i++){
