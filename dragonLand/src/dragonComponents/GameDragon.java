2813734ac842a4fd2a057e4af0264367cd456966
Student8 <Student8@10.8.33.154>
2017-02-14 13:50:52 -0500

    Tried to fix dragon display, no real progress.

--- dragonLand/src/dragonComponents/GameDragon.java
+++ dragonLand/src/dragonComponents/GameDragon.java
@@ -11,31 +11,21 @@ import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 
 import guiPractice.components.AnimatedComponent;
+import guiPractice.components.Graphic;
 import guiPractice.components.MovingComponent;
 
-public class GameDragon extends AnimatedComponent{
+public class GameDragon extends Graphic{
 	
-	private String imgSrc;
+	//private String imgSrc;
 	
 	//must add picture and animate
 	
 	public GameDragon(int x, int y, int w, int h, String imgSrc) {
-		super(x, y, w, h);
-		setX(x);
+		super(10, 10, 200, 200, imgSrc);
+		System.out.println("star");
+		//setX(getRandomX());
 		setY(y);
-		this.imgSrc=imgSrc;
+//		vy = 0;
 	}
 
-	@Override
-	public void checkBehaviors() {
-		if(true){
-			if(currentFrame==2)
-				currentFrame=0;
-		}
-	}
-
-	@Override
-	public void drawImage(Graphics2D g){
-		super.drawImage(g);
-	}
 }
--- dragonLand/src/game/GameVioletta.java
+++ dragonLand/src/game/GameVioletta.java
@@ -36,7 +36,7 @@ public class GameVioletta {
 		return dragonArray;
 	}
 	
-	public static void addDragon(ArrayList<Visible> viewObjects, String imgSrc){
+	public static void addDragon(String imgSrc){
 		dragonArray = new ArrayList<GameDragon>();
 		int dragonHeight = 50;
 		
@@ -44,46 +44,12 @@ public class GameVioletta {
 		int yPos = screenHeight - dragonHeight;
 		
 		//GameDragon temp = new GameDragon(xPos/2, (int)(yPos*0.75), 200, 200, "dragon1.png");
-		GameDragon temp = new GameDragon(100, 100, 200, 200, imgSrc);
-		
-		addAnimation(viewObjects, imgSrc, temp);
+		GameDragon temp = new GameDragon(100, 100, 100, 100, imgSrc);
 		
 		dragonArray.add(temp);
 	}
 
-	private static void addAnimation(ArrayList<Visible> viewObjects, String imgSrc, GameDragon temp) {
-		//Kat code
-				try{
-					ImageIcon icon = new ImageIcon(imgSrc);
-					int numberRow =3 ;
-					int rows =4;
-					int w =48;
-					int h = 48;
-					for(int i=0;i<numberRow*rows;i++){
-						
-						//declare cropped image
-						BufferedImage cropped = new BufferedImage(w,h,BufferedImage.TYPE_INT_ARGB);
-						int leftMargin=0;
-						int topMargin =0 ;
-						int x1 = leftMargin + w*(i%numberRow);
-						int y1=topMargin +h*(i/numberRow);
-						Graphics g = cropped.createGraphics();
-						g.drawImage(icon.getImage(),0,0,w,h,x1,y1,x1+w,y1+h,null);
-						temp.addFrame(cropped, 300);
-						if(i==numberRow*rows-1)
-							i++;
-					}
-				}
-				catch(Exception e){
-					e.printStackTrace();
-				}
-				viewObjects.add(temp);
-				temp.update();
-				//temp.setX(100);
-				//temp.setY(100);
-				
-				temp.play();
-	}
+	
 
 	public static void changeDragonPos(int i) {
 		int leadDragon = findLeadDragon(i);
