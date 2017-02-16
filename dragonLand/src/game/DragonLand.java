460b3884760e2f9a445e7695475058ee271bfcb0
Student8 <Student8@10.8.33.154>
2017-02-16 14:02:16 -0500

    nevermind changes

--- dragonLand/src/game/DragonLand.java
+++ dragonLand/src/game/DragonLand.java
@@ -63,7 +63,7 @@ public class DragonLand extends GUIApplication {
 	@Override
 	protected void initScreen() {
 		initColors();
-		//homeScreen = new HomeScreen(getWidth(),getHeight());
+		homeScreen = new HomeScreen(getWidth(),getHeight());
 		miniGameScreen = new GameScreen(getWidth(),getHeight());
 		//System.out.println(miniGameScreen.getWidth());
 //		shopMain = new (getWidth(),getHeight());
@@ -72,7 +72,7 @@ public class DragonLand extends GUIApplication {
 		//highscoreScreen = new HighScoreScreen(getWidth(),getHeight());
 //		miniGameScreen = new (getWidth(),getHeight());
 		//uncomment your line once u have a class, input class name before get width()/height()
-		setScreen(miniGameScreen);
+		setScreen(homeScreen);
 		//////////!!!!!!!!! if u want to test only your screen change the above lines^
 		// but before you push to develop/ merge from develop always change it back plz
 	}
