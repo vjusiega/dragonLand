497eb2f02291d187f5a6e744a1f0cf7f65369251
Student6 <Student6@10.8.33.148>
2017-02-16 14:02:45 -0500

    trying to converge into only one screen

--- dragonLand/src/game/BuyScreenWendy.java
+++ dragonLand/src/game/BuyScreenWendy.java
@@ -37,6 +37,7 @@ public class BuyScreenWendy extends ShopScreen{
 	@Override
 	public void addDragonLabels(ArrayList<Visible> visible) {
 		// TODO Auto-generated method stub
+		inLists(3);
 		pageNum = 1;
 		getArrowLeft().setAction(new Action(){
 
@@ -52,15 +53,16 @@ public class BuyScreenWendy extends ShopScreen{
 			@Override
 			public void act() {
 				// TODO Auto-generated method stub
-				pageNum = 2;
+				//DragonLand.game.setScreen(DragonLand.game.buyScreen2);
+				inLists(6);
+				System.out.println(dragonsInShop.size());
+				pageNum++;
 				System.out.println(pageNum);
-				DragonLand.game.setScreen(DragonLand.game.buyScreen2);
 				update();
 			}
 			
 		});
 
-		inLists();
 		
 //		sold = SellShopZheng.getSold();
 //		if(sold != null)
@@ -122,7 +124,7 @@ public class BuyScreenWendy extends ShopScreen{
 	}
 	
 	
-	public void inLists(){
+	public void inLists(int num){
 		
 		dragonsInShop = new ArrayList<Dragon>();
 		dragons = new ArrayList<Dragon>();
@@ -145,26 +147,24 @@ public class BuyScreenWendy extends ShopScreen{
 				dragonsInShop.add(dragons.get(i));
 			}
 //		}
-
-
 		
 	}
 	
-	public static int getNum(){
-		return num;
-	}
-
-	public static int getNumOfDragon()
-	{
-		return numOfDragons;
-	}
-	
-	public static void setNumOfDragon()
-	{
-		numOfDragons ++;
-	}
-	
-	public static int getPageNum(){
-		return pageNum;
-	}
+//	public static int getNum(){
+//		return num;
+//	}
+//
+//	public static int getNumOfDragon()
+//	{
+//		return numOfDragons;
+//	}
+//	
+//	public static void setNumOfDragon()
+//	{
+//		numOfDragons ++;
+//	}
+//	
+//	public static int getPageNum(){
+//		return pageNum;
+//	}
 }
