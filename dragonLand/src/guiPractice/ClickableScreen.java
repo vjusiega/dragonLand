8b3e17036f8146c3988351eb1e4b5362cd6f8440
Nockles <Nockles@10.8.33.99>
2017-02-16 13:49:10 -0500

    changes

--- dragonLand/src/guiPractice/ClickableScreen.java
+++ dragonLand/src/guiPractice/ClickableScreen.java
@@ -40,7 +40,7 @@ public abstract class ClickableScreen extends Screen implements MouseListener {
 	public void mouseClicked(MouseEvent e) {
 		for(int i=0; i<clickables.size();i++){
 			Clickable c= clickables.get(i);
-			if(c.isHovered(e.getX(), e.getY())){
+			if(c.getAction() != null && c.isHovered(e.getX(), e.getY())){
 				c.act();
 				break;
 			}
