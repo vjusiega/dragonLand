d56110c2acf15642e69b2ab8d3ff656b1847df0f
Veeraj Jethalal <veera@DESKTOP-0ECFG49>
2017-02-26 18:57:50 -0500

    s

--- dragonLand/src/game/DragonLand.java
+++ dragonLand/src/game/DragonLand.java
@@ -23,22 +23,26 @@ public class DragonLand extends GUIApplication {
 	/**
 	 * Static Fields
 	 */
+
 	public static DragonLand game;
+
 	//public static ArrayList<AnimatedComponent> dragonList; //list of all dragons in the game
 	public static int coins; 
+
 	public static Screen homeScreen;
 	public static Screen shopMain; // shop 1
 	public static Screen sellScreen; // shop 2
 	public static Screen buyScreen; // shop 3
 	public static Screen highscoreScreen; // high score
 	public static Screen miniGameScreen; // minigame
+
 	public static Screen HelpScreen;
+
 	public static Color NAVY;
 	public static Color BRIGHT_PINK;
 	public static Color LIGHT_PINK;
 	public static Color LIGHT_NUDE;
 	public static Color DARKER_NUDE;
-	
 	
 	
 	/**
@@ -64,18 +68,15 @@ public class DragonLand extends GUIApplication {
 	@Override
 	protected void initScreen() {
 		initColors();
-//		homeScreen = new HomeScreen(getWidth(),getHeight());
-//		miniGameScreen = new GameScreen(getWidth(),getHeight());
-		//System.out.println(miniGameScreen.getWidth());
-//		shopMain = new (getWidth(),getHeight());
-//		sellScreen = new (getWidth(),getHeight());
-//		buyScreen = new (getWidth(),getHeight());
+		homeScreen = new HomeScreen(getWidth(),getHeight());
+		miniGameScreen = new GameScreen(getWidth(),getHeight());
+		shopMain = new HomeShopScreen(getWidth(),getHeight());
+		//sellScreen = new (getWidth(),getHeight());
+		buyScreen = new BuyScreenWendy(getWidth(),getHeight());
 		highscoreScreen = new HighScoreScreen(getWidth(),getHeight());
-//		miniGameScreen = new (getWidth(),getHeight());
-		//uncomment your line once u have a class, input class name before get width()/height()
-		setScreen(highscoreScreen);
-		//////////!!!!!!!!! if uwant to test only your screen change the above lines^
-		// but before you push to develop/ merge from develop always change it back plz
+		
+		setScreen(homeScreen);
+
 	}
 
 	private void initColors() {
@@ -95,5 +96,13 @@ public class DragonLand extends GUIApplication {
 		Thread go = new Thread(game);
 		go.start();
 	}
+	
+	//public coin getter + setter
+		public void setCoins(int x){
+			coins = x;
+		}
+		public int getCoins(){
+			return coins;
+		}
 
 }
