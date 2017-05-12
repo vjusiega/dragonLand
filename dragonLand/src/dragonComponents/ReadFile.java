4549abcadffc32d24bad8b66ea341633474b0231
Student 8 <Student 8@10.8.33.158>
2017-05-12 13:55:40 -0400

    Started readFile class

---
+++ dragonLand/src/dragonComponents/ReadFile.java
@@ -0,0 +1,45 @@
+package dragonComponents;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+
+public class ReadFile {
+
+	private String path;
+	
+	public ReadFile(String filePath) {
+		path = filePath;
+	}
+	
+	public String[] OpenFile() throws IOException{
+		FileReader fr = new FileReader(path);
+		BufferedReader textReader = new BufferedReader(fr);
+		
+		int numOfLines = 3;
+		String[] textData= new String[numOfLines];
+		
+		for(int i=0; i<numOfLines;  i++){
+			textData[i] = textReader.readLine();
+		}
+		
+		textReader.close();
+		return textData;
+	}
+	
+	public int readLines() throws IOException{
+		FileReader fileRead = new FileReader(path);
+		BufferedReader bf = new BufferedReader(fileRead);
+		
+		int numOfLines=0;
+		String aLine = "";
+		
+		while((aLine = bf.readLine())!=null){
+			numOfLines++;
+		}
+		
+		return numOfLines;
+	}
+
+}
