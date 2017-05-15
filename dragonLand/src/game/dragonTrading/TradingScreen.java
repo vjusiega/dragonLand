2c2bd8fb1dccb5070ae8d3cf0d6ea70a6d8372eb
Student 8 <Student 8@10.8.33.169>
2017-05-15 12:53:30 -0400

    not much

---
+++ dragonLand/src/game/dragonTrading/TradingScreen.java
@@ -0,0 +1,30 @@
+package game.dragonTrading;
+
+import java.util.ArrayList;
+
+import guiPractice.Screen;
+import guiPractice.components.Visible;
+
+public class TradingScreen extends Screen {
+
+	
+	public TradingScreen(int width, int height) {
+		super(width, height);
+	}
+
+	@Override
+	public void initObjects(ArrayList<Visible> viewObjects2) {
+
+	}
+
+}
+/**
+	your dragons to sell
+	
+	dragons to buy
+		users trading dragons
+	
+	
+	
+	
+**/
