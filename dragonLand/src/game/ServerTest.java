8324ad0351b03af9db4ea638a019bb4a173c84fe
Student 8 <Student 8@10.8.33.169>
2017-05-16 13:54:09 -0400

    started editing server class

---
+++ dragonLand/src/game/ServerTest.java
@@ -0,0 +1,13 @@
+package game;
+import javax.swing.JFrame;
+
+
+public class ServerTest {
+
+	public static void main(String[] args) {
+		Server sally = new Server();
+		sally.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		sally.startRunning();
+	}
+
+}
