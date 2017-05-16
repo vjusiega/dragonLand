5021cf71dd119c9da9a7bda0f049bc584a566d93
violettajusiega <violettajusiega@192.168.0.7>
2017-05-15 21:16:26 -0400

    Clickable post

--- dragonLand/src/introScreens/WelcomeScreen.java
+++ dragonLand/src/introScreens/WelcomeScreen.java
@@ -14,6 +14,7 @@ import guiPractice.ClickableScreen;
 import guiPractice.components.Action;
 import guiPractice.components.AnimatedComponent;
 import guiPractice.components.Button;
+import guiPractice.components.ClickableGraphic;
 import guiPractice.components.Graphic;
 import guiPractice.components.GraphicMovingComponent;
 import guiPractice.components.Visible;
@@ -36,22 +37,33 @@ public class WelcomeScreen extends ClickableScreen{
 		viewObjects.add(setUpDragons(1));
 		viewObjects.add(setUpDragons(3));
 		
-		Button exit = new Button(getWidth()-135, getHeight()-60, 120, 50, "Continue...", DragonLand.DARKER_NUDE, new Action() {
-			@Override
-			public void act() {
+		ClickableGraphic post = new ClickableGraphic(getWidth()-250, getHeight()-300, 1.0,"img/oneSignLeft.png");
+		post.setAction(new Action(){
+			public void act(){
 				DragonLand.game.setScreen(DragonLand.homeScreen);
 			}
 		});
-		viewObjects.add(exit);
 		
-		setUpFog();
+//		Button exit = new Button(getWidth()-135, getHeight()-60, 120, 50, "Continue...", DragonLand.DARKER_NUDE, new Action() {
+//			@Override
+//			public void act() {
+//				DragonLand.game.setScreen(DragonLand.homeScreen);
+//			}
+//		});
+//		viewObjects.add(exit);
+		
+		setUpFog(post);
+	
 	}
 
-	public void setUpFog(){
+	public void setUpFog(ClickableGraphic post){
 		Fog fog; 
 		
 		for(int i = -10; i < 10; i++){
 			fog = new Fog((i*getWidth() / 10), 200, 500, 300, "img/introFog.png", 100);
+			if(i == 8){
+				viewObjects.add(post);
+			}
 			viewObjects.add(fog);
 			fog.setY(fog.generateYPos());
 			fog.play();
