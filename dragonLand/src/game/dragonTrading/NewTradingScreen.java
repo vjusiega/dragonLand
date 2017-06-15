91d3291f6f1e8b38d77edae453eb00cf1a3ddeeb
violettajusiega <violettajusiega@192.168.0.7>
2017-06-15 15:58:41 -0400

    stuff

--- dragonLand/src/game/dragonTrading/NewTradingScreen.java
+++ dragonLand/src/game/dragonTrading/NewTradingScreen.java
@@ -104,7 +104,7 @@ public class NewTradingScreen extends ClickableScreen implements Runnable{
 			public void act(){
 				Thread Client = new Thread(new Runnable(){
 					public void run(){
-						Client c = new Client("127.0.0.1");
+						Client c = new Client("172.20.10.2");
 						c.startRunning(thisScreen);
 					}
 				});
